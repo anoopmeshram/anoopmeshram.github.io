f6f3c750bc29af275b498527cd53b8ef21fa1401
Anoop Meshram <88427353+anoopmeshram@users.noreply.github.com>
2026-03-27 15:51:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -352,9 +352,9 @@ class RetroFutureLoginForm {
         setTimeout(() => {
             console.log('Portal activated! Welcome to the future...');
             window.location.href = 'https://anoopmeshram.github.io/slcp';
-        }, 10000); // fast redirect
+        }, 1000); 
     }
-    
+   // fast redirect 
     triggerPortalEffects() {
         // Portal activation effects
         const card = document.querySelector('.future-card');
