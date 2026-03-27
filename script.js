cdcddbf28eb7d087132c230b7d644c9970908648
Anoop Meshram <88427353+anoopmeshram@users.noreply.github.com>
2026-03-27 15:55:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ class RetroFutureLoginForm {
         setTimeout(() => {
             console.log('Portal activated! Welcome to the future...');
             window.location.href = 'https://anoopmeshram.github.io/slcp';
-        }, 3000); 
+        }, 5000); 
     }
    // fast redirect 
     triggerPortalEffects() {
